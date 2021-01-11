2c3932324fb858cd5dd0b0a90cacf04ed26309ed
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2021-01-11 17:08:00 +0800

    Adds new file

---
+++ coding_exam.rb
@@ -0,0 +1,38 @@
+# Write a function called FooBar that takes input integer n and prints all the numbers from 1 up to n in a new line. If the number is divisible by 3, then print "Foo"; if the number is divisible by 5, then print "Bar"; and if the number is divisible by both 3 and 5, then print "FooBar". Otherwise, just print the number.
+
+# Sample Input:
+# 15
+
+# Sample Output:
+# 1
+# 2
+# Foo
+# 4
+# Bar
+# Foo
+# 7
+# 8
+# Foo
+# Bar
+# 11
+# Foo
+# 13
+# 14
+# FooBar
+
+def FooBar(number)
+  (1..number).to_a.map do |x|
+    if x % 5 == 0 && x % 3 == 0
+    p "FooBar"
+    elsif x % 3 == 0
+      p "Foo"
+    elsif x % 5 == 0
+      p "Bar"
+    else
+      p x
+    end
+  end
+end
+
+FooBar(30)
+
